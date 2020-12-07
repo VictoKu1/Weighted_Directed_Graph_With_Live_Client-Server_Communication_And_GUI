299cdb03e95da72b37422243dc4562e1ad9f0b16
porat-ah <porat2001@gmail.com>
2020-12-07 23:08:07 +0200

    added: DWGraph_DSAlgoTest

---
+++ ex2/src/tests/DWGraph_DSAlgoTest.java
@@ -0,0 +1,70 @@
+package tests;
+import api.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DWGraph_DSAlgoTest {
+    dw_graph_algorithms ag;
+    DWGraph_DS graph_ds;
+    @BeforeEach
+    void setup(){
+        graph_ds = new DWGraph_DS();
+        for (int i = 0; i < 5; i++) {
+            node_data n = new node_test(i);
+            graph_ds.addNode(n);
+        }
+        graph_ds.connect(0,1,1);
+        graph_ds.connect(0,2,2);
+        graph_ds.connect(1,3,3);
+        graph_ds.connect(2,4,1);
+        graph_ds.connect(4,3,1);
+        graph_ds.connect(3,0,1);
+    }
+    @Test
+    void getGraph(){
+        assertEquals(null,ag.getGraph());
+        ag.init(graph_ds);
+        assertEquals(graph_ds,ag.getGraph());
+    }
+    @Test
+    void copy(){
+        ag.init(graph_ds);
+        assertEquals(graph_ds,ag.copy());
+    }
+    @Test
+    void isConnected(){
+        ag.init(graph_ds);
+        assertEquals(true,ag.isConnected());
+        graph_ds.removeEdge(3,0);
+        assertEquals(false,ag.isConnected());
+    }
+    @Test
+    void shortestPathDist(){
+        ag.init(graph_ds);
+        assertEquals(3,ag.shortestPathDist(0,3));
+        assertEquals(-1,ag.shortestPathDist(0,100));
+        assertEquals(-1,ag.shortestPathDist(100,100));
+        assertEquals(-1,ag.shortestPathDist(101,100));
+        assertEquals(-1,ag.shortestPathDist(101,0));
+    }
+    @Test
+    void shortestPath(){
+        ag.init(graph_ds);
+        //assertEquals(,ag.shortestPath(0,3));
+        assertEquals(null,ag.shortestPath(0,100));
+        assertEquals(null,ag.shortestPath(100,100));
+        assertEquals(null,ag.shortestPath(101,100));
+        assertEquals(null,ag.shortestPath(101,0));
+    }
+    @Test
+    void save(){
+        ag.init(graph_ds);
+        assert ag.save("test.json");
+    }
+    @Test
+    void load(){
+        ag.init(graph_ds);
+        assert ag.load("test.json");
+        assertEquals(graph_ds,ag.getGraph());
+    }
+}
---
+++ ex2/src/tests/node_test.java
@@ -0,0 +1,108 @@
+package tests;
+
+import api.geo_location;
+import api.node_data;
+
+public class node_test implements node_data {
+    int key;
+    geo_location location;
+    double weight;
+    String info;
+    int tag;
+
+    public node_test(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public int getKey() {
+        return key;
+    }
+
+    /**
+     * Returns the location of this node, if
+     * none return null.
+     *
+     * @return
+     */
+    @Override
+    public geo_location getLocation() {
+        return location;
+    }
+
+    /**
+     * Allows changing this node's location.
+     *
+     * @param p - new new location  (position) of this node.
+     */
+    @Override
+    public void setLocation(geo_location p) {
+        this.location = p;
+
+    }
+
+    /**
+     * Returns the weight associated with this node.
+     *
+     * @return
+     */
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Allows changing this node's weight.
+     *
+     * @param w - the new weight
+     */
+    @Override
+    public void setWeight(double w) {
+        this.weight = w;
+
+    }
+
+    /**
+     * Returns the remark (meta data) associated with this node.
+     *
+     * @return
+     */
+    @Override
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * Allows changing the remark (meta data) associated with this node.
+     *
+     * @param s
+     */
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+
+    }
+
+    /**
+     * Temporal data (aka color: e,g, white, gray, black)
+     * which can be used be algorithms
+     *
+     * @return
+     */
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    /**
+     * Allows setting the "tag" value for temporal marking an node - common
+     * practice for marking by algorithms.
+     *
+     * @param t - the new value of the tag
+     */
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+
+    }
+}
